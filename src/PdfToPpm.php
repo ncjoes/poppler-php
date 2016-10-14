0e59c558fc60eb0465caf4e46d486c45fb6f40df
Chukwuemeka Nwobodo <jc.nwobodo@gmail.com>
2016-10-14 17:29:11 +0100

    Setup stub classes (wrappers) for remaining Poppler-utils

---
+++ src/PdfToPpm.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * PHP-Poppler
+ *
+ * Author:  Chukwuemeka Nwobodo (jcnwobodo@gmail.com)
+ * Date:    10/14/2016
+ * Time:    3:35 PM
+ **/
+
+namespace NcJoes\PhpPoppler;
+
+class PdfToPpm extends PopplerUtil
+{
+    public function utilOptions()
+    {
+        // TODO: Implement utilOptions() method.
+    }
+
+    public function utilOptionRules()
+    {
+        // TODO: Implement utilOptionRules() method.
+    }
+
+    public function utilFlags()
+    {
+        // TODO: Implement utilFlags() method.
+    }
+
+    public function utilFlagRules()
+    {
+        // TODO: Implement utilFlagRules() method.
+    }
+
+}
---
+++ src/PdfToText.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * PHP-Poppler
+ *
+ * Author:  Chukwuemeka Nwobodo (jcnwobodo@gmail.com)
+ * Date:    10/14/2016
+ * Time:    3:36 PM
+ **/
+
+namespace NcJoes\PhpPoppler;
+
+class PdfToText extends PopplerUtil
+{
+    public function utilOptions()
+    {
+        // TODO: Implement utilOptions() method.
+    }
+
+    public function utilOptionRules()
+    {
+        // TODO: Implement utilOptionRules() method.
+    }
+
+    public function utilFlags()
+    {
+        // TODO: Implement utilFlags() method.
+    }
+
+    public function utilFlagRules()
+    {
+        // TODO: Implement utilFlagRules() method.
+    }
+
+}
