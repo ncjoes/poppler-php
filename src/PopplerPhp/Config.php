80f6e9698e09e72e52450ba4cb737dc669faf07d
Chukwuemeka Nwobodo <jc.nwobodo@gmail.com>
2016-10-15 17:53:29 +0100

    Relocate test files

--- src/PopplerPhp/Config.php
+++ src/PopplerPhp/Config.php
@@ -69,7 +69,7 @@ class Config
     public static function getOutputDirectory($default = null)
     {
         $check = is_dir($default);
-        $default = $check ? $default : H::parseDirName(__DIR__.'/../../tests/PopplerPhp/results');
+        $default = $check ? $default : H::parseDirName(__DIR__.'/../../tests/results');
 
         return Config::get(C::OUTPUT_DIR, realpath($default));
     }
