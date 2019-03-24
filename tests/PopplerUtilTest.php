9af7864bd2e871dd7e330bfed1ea439480e67143
hiraro <traurig.orz@gmail.com>
2019-03-25 01:18:05 +0900

    modify makeShellCommand test

--- tests/PopplerUtilTest.php
+++ tests/PopplerUtilTest.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Poppler-PHP
  *
@@ -6,7 +7,6 @@
  * Date:    10/13/2016
  * Time:    2:31 PM
  **/
-
 use NcJoes\PopplerPhp\Config;
 use NcJoes\PopplerPhp\Constants as C;
 use NcJoes\PopplerPhp\PdfInfo;
@@ -14,6 +14,7 @@ use NcJoes\PopplerPhp\PdfToCairo;
 
 class PopplerUtilTest extends PHPUnit_Framework_TestCase
 {
+
     public function setUp()
     {
         parent::setUp();
@@ -64,14 +65,49 @@ class PopplerUtilTest extends PHPUnit_Framework_TestCase
 
     public function testMakeCommandMethod()
     {
+        $q = PHP_OS === 'WINNT' ? "\"" : "'";
         $DS = DIRECTORY_SEPARATOR;
         $file = __DIR__.$DS."sources{$DS}test1.pdf";
-        $pdf = new PdfToCairo($file);
+        $output_file_prefix = 'test-file';
 
-        $pdf->setOutputFilenamePrefix('test-file');
-        $this->assertTrue(str_contains($pdf->previewShellCommand(), $pdf->getOutputFilenamePrefix()));
-        //$this->assertTrue(str_contains($pdf->previewShellCommand(), $pdf->binDir()));
-        $this->assertTrue(str_contains($pdf->previewShellCommand(), $pdf->getOutputSubDir()));
+        $pdf = new PdfToCairo($file);
+        $pdf->setOutputFilenamePrefix($output_file_prefix);
+
+        $bin_dir = Config::getBinDirectory();
+        $bin_file = C::PDF_TO_CAIRO;
+        $output_dir = Config::getOutputDirectory();
+
+        $expected_bin = "{$q}{$bin_dir}{$DS}{$bin_file}{$q}";
+        $expected_src = "{$q}{$file}{$q}";
+        $expected_dest = "{$q}{$output_dir}{$DS}{$output_file_prefix}{$q}";
+        $this->assertRegExp(
+            "%^{$expected_bin} {$expected_src} {$expected_dest}$%",
+            $pdf->previewShellCommand()
+        );
     }
 
+    public function testMakeCommandMethodWithSubDirEnabled()
+    {
+        $q = PHP_OS === 'WINNT' ? "\"" : "'";
+        $DS = DIRECTORY_SEPARATOR;
+        $file = __DIR__.$DS."sources{$DS}test1.pdf";
+        $output_file_prefix = 'test-file';
+
+        $pdf = new PdfToCairo($file);
+        $pdf->setSubDirRequired(true);
+        $pdf->setOutputFilenamePrefix($output_file_prefix);
+
+        $bin_dir = Config::getBinDirectory();
+        $bin_file = C::PDF_TO_CAIRO;
+        $output_dir = Config::getOutputDirectory();
+        $output_sub_dir = $pdf->getOutputSubDir();
+
+        $expected_bin = "{$q}{$bin_dir}{$DS}{$bin_file}{$q}";
+        $expected_src = "{$q}{$file}{$q}";
+        $expected_dest = "{$q}{$output_dir}{$DS}{$output_sub_dir}{$DS}{$output_file_prefix}{$q}";
+        $this->assertRegExp(
+            "%^{$expected_bin} {$expected_src} {$expected_dest}$%",
+            $pdf->previewShellCommand()
+        );
+    }
 }
